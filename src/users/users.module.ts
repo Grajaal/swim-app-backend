5b82fd714dee80ea4f0c7408a229916b84a91001
Grajaal <jorgegrajal@gmail.com>
2025-03-22 17:01:05 +0100

    feat(users): update user module and service logic

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { PrismaService } from 'src/prisma.service';
+import { Module } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { PrismaService } from 'src/prisma.service'
 
 @Module({
   providers: [UsersService, PrismaService],
-  exports: [UsersService],
+  exports: [UsersService]
 })
-export class UsersModule { }
+export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,30 @@
-
-import { Injectable } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
-import { PrismaService } from 'src/prisma.service';
+import { ConflictException, Injectable } from '@nestjs/common'
+import { Prisma, User } from '@prisma/client'
+import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 export class UsersService {
-  constructor(private db: PrismaService) { }
+  constructor(private db: PrismaService) {}
 
   async user(
-    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput
   ): Promise<User | null> {
-    return this.db.user.findUnique({
-      where: userWhereUniqueInput,
+    return await this.db.user.findUnique({
+      where: userWhereUniqueInput
     })
   }
 
   async createUser(data: Prisma.UserCreateInput): Promise<User> {
+    const user = await this.db.user.findUnique({
+      where: { email: data.email }
+    })
+
+    if (user) {
+      throw new ConflictException('Email is already in use')
+    }
+
     return this.db.user.create({
-      data,
+      data
     })
   }
 }
