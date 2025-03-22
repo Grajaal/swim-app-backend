bcbdf89e01f41f0a4f667203d45229696f3836f8
Grajaal <jorgegrajal@gmail.com>
2025-03-22 17:01:27 +0100

    refactor: update application bootstrap logic

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as cookieParser from 'cookie-parser'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Swim App')
+    .setDescription('App for swimming coaches')
+    .setVersion('1.0')
+    .addTag('swim')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, documentFactory)
+
+  app.setGlobalPrefix('api')
+  app.use(cookieParser())
   app.enableCors({
     origin: 'http://localhost:3000',
     methods: 'GET, POST, PUT, DELETE',
     allowedHeaders: 'Content-Type, Authorization',
-    credentials: true,
+    credentials: true
   })
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 4001)
 }
-bootstrap();
+bootstrap().catch((err) =>
+  console.error('Error durante el inicio de la aplicación:', err)
+)
