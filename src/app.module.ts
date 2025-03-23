6d1497820639109423368558e00583fb20c64f2a
Grajaal <jorgegrajal@gmail.com>
2025-03-23 19:54:47 +0100

    feat: add swimmers and coaches modules to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service'
 import { PrismaService } from './prisma.service'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
+import { SwimmersModule } from './swimmers/swimmers.module';
+import { CoachesModule } from './coaches/coaches.module';
 
 @Module({
-  imports: [UsersModule, AuthModule],
+  imports: [UsersModule, AuthModule, SwimmersModule, CoachesModule],
   controllers: [AppController],
   providers: [AppService, PrismaService]
 })
