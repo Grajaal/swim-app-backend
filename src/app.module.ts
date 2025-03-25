f4d768791e5ba0101124453705752c2571abbd07
Grajaal <jorgegrajal@gmail.com>
2025-03-25 21:18:06 +0100

    feat(teams): create teams module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,20 @@ import { AppService } from './app.service'
 import { PrismaService } from './prisma.service'
 import { UsersModule } from './users/users.module'
 import { AuthModule } from './auth/auth.module'
-import { SwimmersModule } from './swimmers/swimmers.module';
-import { CoachesModule } from './coaches/coaches.module';
+import { SwimmersModule } from './swimmers/swimmers.module'
+import { CoachesModule } from './coaches/coaches.module'
+import { TeamsService } from './teams/teams.service'
+import { TeamsModule } from './teams/teams.module'
 
 @Module({
-  imports: [UsersModule, AuthModule, SwimmersModule, CoachesModule],
+  imports: [
+    UsersModule,
+    AuthModule,
+    SwimmersModule,
+    CoachesModule,
+    TeamsModule
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService]
+  providers: [AppService, PrismaService, TeamsService]
 })
 export class AppModule {}
