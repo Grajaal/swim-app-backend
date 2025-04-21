f40e57f560f1765e88a6b158ec32e0b518e3691c
Grajal <jgrajh00@estudiantes.unileon.es>
2025-04-21 16:59:32 +0200

    chore(teams): add SwimmersService to teams module providers
    - Import SwimmersService for team-related swimmer operations
    - Add SwimmersService to module providers

--- src/teams/teams.module.ts
+++ src/teams/teams.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common'
 import { TeamsController } from './teams.controller'
 import { TeamsService } from './teams.service'
 import { PrismaService } from 'src/prisma.service'
+import { SwimmersService } from 'src/swimmers/swimmers.service'
 
 @Module({
   controllers: [TeamsController],
-  providers: [TeamsService, PrismaService]
+  providers: [TeamsService, PrismaService, SwimmersService]
 })
 export class TeamsModule {}
