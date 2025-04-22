3f61ef2864779f442d34efad14c4cd23bcb8829c
Grajaal <jorgegrajal@gmail.com>
2025-04-22 17:56:08 +0200

    refactor: change setHours to setUTCHours for consistency

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -105,10 +105,10 @@ export class GroupsService {
 
     if (date) {
       const startOfDay = new Date(date)
-      startOfDay.setHours(0, 0, 0, 0)
+      startOfDay.setUTCHours(0, 0, 0, 0)
 
       const endOfDay = new Date(date)
-      endOfDay.setHours(23, 59, 59, 999)
+      endOfDay.setUTCHours(23, 59, 59, 999)
 
       return await this.db.training.findMany({
         where: {
